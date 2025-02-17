dbdb7a3c1137b48f620050316b39c1b0ad79b312
Hojin Jang <23309485+jhojin7@users.noreply.github.com>
2025-02-17 23:16:46 +0900

    ,

---
+++ test.py
@@ -0,0 +1,46 @@
+# Run: pytest test.py
+import pytest
+from pymongo import MongoClient
+
+
+def get_db():
+    TEST_MONGODB_URL = (
+        "mongodb://localhost:27017/test_db"  # Replace with your test DB URL
+    )
+    client = MongoClient(TEST_MONGODB_URL)
+    db = client.get_database()  # Or db = client["test_db"]
+    collection = db["items"]  # Or db.get_collection("items")
+    return collection
+
+
+def do():
+    print("do")
+
+
+def test_do():
+    do()
+    assert 1 == 1
+
+
+def test_insert():
+    collection = get_db()
+    collection.insert_one({"name": "test"})
+    assert collection.count_documents({}) == 1
+
+
+def tset_insert_fail():
+    collection = get_db()
+    collection.insert_one({"name": "test"})
+    assert collection.count_documents({}) == 2
+
+
+def test_delete_success():
+    collection = get_db()
+    collection.delete_one({"name": "test"})
+    assert collection.count_documents({}) == 0
+
+
+def test_delete_fail():
+    collection = get_db()
+    collection.delete_one({"name": "test"})
+    assert collection.count_documents({}) == 0
